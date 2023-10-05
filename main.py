717be65db7a8247234ec15328dbcc06605ef5bd9
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-05 00:10:53 -0600

    Just for training.

---
+++ main.py
@@ -0,0 +1,11 @@
+var = "h ello"
+
+print(var)
+print(var.lower())
+print(var.find("o"))
+print(var.title())
+print(var.replace("o", "a"))
+print(var.casefold())
+print(var.capitalize())
+print(var.split(" "))
+print(var.join("e"), "anything")
