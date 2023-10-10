9d2700ec92c92f269841cf768c2e60205c3b89ac
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-10 10:10:08 -0600

    Continue.

--- breaks_continue.py
+++ breaks_continue.py
@@ -2,7 +2,14 @@
 # continue = skips to the next iteration of the loop
 # pass = do nothing, acts as a placeholder
 
-while True:
-    name = input("Enter your name: ")
-    if name != "": # if you dodn;t type in anything it will ask for that input
-        break
+# while True:
+#     name = input("Enter your name: ")
+#     if name != "":  # if you don't type in anything it will ask for that input
+#         break
+
+phone_number = "123-456-789"
+
+for i in phone_number:
+    if i == "-":
+        continue  # this will just skip the iteration (means start the iteration again)
+    print(i, end="")
