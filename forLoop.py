ef64b3bf7f2f40556bac1837b0e96f3c8ce8ed70
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-09 20:26:54 -0600

    Looping backwards with time module and print happy new year.

--- forLoop.py
+++ forLoop.py
@@ -1,10 +1,17 @@
+import time
+
 for i in range(1, 11):  # regular range loop, the last number is not included.
     print(i, end="")
 
 for i in range(10):  # regular range loop
-    print(i+1, end=" ")
+    print(i + 1, end=" ")
 
 name = "Ahmed"
 
-for i in name:
+for i in name:  # looping over string
     print(i, end=",")
+
+for i in range(10, 0, -1):  # looping backwards
+    time.sleep(1)
+    print(i)
+print("Happy new year!")
