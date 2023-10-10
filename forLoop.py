4a53a8816aa2b0d2b39baa12f7886f95c68bae0a
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-09 20:23:56 -0600

    For loop with a string.

--- forLoop.py
+++ forLoop.py
@@ -1,5 +1,10 @@
 for i in range(1, 11):  # regular range loop, the last number is not included.
     print(i, end="")
 
-for i in range(10):
+for i in range(10):  # regular range loop
     print(i+1, end=" ")
+
+name = "Ahmed"
+
+for i in name:
+    print(i, end=",")
