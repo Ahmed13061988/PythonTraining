c1ead09f70716925329e45ec281cef65e9d54e5f
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-10 10:07:29 -0600

    Break.

--- breaks_continue.py
+++ breaks_continue.py
@@ -2,4 +2,7 @@
 # continue = skips to the next iteration of the loop
 # pass = do nothing, acts as a placeholder
 
-
+while True:
+    name = input("Enter your name: ")
+    if name != "": # if you dodn;t type in anything it will ask for that input
+        break
