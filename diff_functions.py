36eda1df355d187d10ce983409b54ce1f338ad5e
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-10 23:54:51 -0600

    Default parameters.

---
+++ diff_functions.py
@@ -0,0 +1,8 @@
+def display3(flight_number, flight_make="Boeing", seating_capacity=150):
+    print("Flight Number:", flight_number)
+    print("Flight Make:", flight_make)
+    print("Seating Capacity:", seating_capacity)
+
+
+display3("FN234")
+display3("FN678", "Qantas", 200)
