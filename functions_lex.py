69b4802cece53d78423018655d3025f5452f6126
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-10 23:13:12 -0600

    adding integers of the number, example 5: 5 + 4+ 3 + 2 + 1

--- functions_lex.py
+++ functions_lex.py
@@ -5,3 +5,15 @@ def find_square(n):
 square = find_square(4)
 
 print(square)
+
+
+def find_sum(n):
+    sum_of_number = n
+    while n > 0:
+        n -= 1
+        sum_of_number += n
+    return sum_of_number
+
+
+sum_1 = find_sum(5)
+print(sum_1)
