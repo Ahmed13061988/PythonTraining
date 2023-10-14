c35bae536ef5d560b5706c14f2e200a1ccc93654
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-13 19:21:26 -0600

    Palindrome

---
+++ is_palindrome.py
@@ -0,0 +1,16 @@
+def is_palindrome(num):
+    boolean = True
+    backwards = []
+    num_str = str(num)
+    original = list(num_str)
+    for n in range(len(num_str) - 1, -1, -1):
+        backwards.append(num_str[n])
+    for i in range(len(original) - 1):
+        if original[i] != backwards[i]:
+            boolean = False
+    return boolean
+
+
+print(is_palindrome(10011))
+
+
