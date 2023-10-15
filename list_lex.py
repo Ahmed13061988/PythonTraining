42e81d1726ba7400c4a9eaa0a32842df5dc1f58e
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-14 20:20:32 -0600

    Another approach to find count of similar elements in on list

--- list_lex.py
+++ list_lex.py
@@ -1,12 +1,18 @@
+# def check(nums):
+#     result = 0
+#     for i in nums:
+#         value = i
+#         nums.remove(i)
+#         if value in nums:
+#             result += 1
+#     return result
+
 def check(nums):
-    result = 0
-    for i in nums:
-        value = i
-        nums.remove(i)
-        if value in nums:
-            result += 1
-    return result
+    count = 0
+    for i in range(len(nums)):
+        if nums[i] == nums[i - 1]:
+            count += 1
+    return count
 
 
-
-print(check([10, 20, 30, 40, 30, 20]))
+print(check([1, 1, 5, 100, -20, -20, 6, 0, 0]))
