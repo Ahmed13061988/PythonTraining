7204c999dc0b618f1b9800265e6f28b64a3d596a
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-14 20:15:04 -0600

    Find adjacent occurrences

--- list_lex.py
+++ list_lex.py
@@ -1,12 +1,11 @@
-ordinary_list = []
+def check(nums):
+    result = 0
+    for i in nums:
+        value = i
+        nums.remove(i)
+        if value in nums:
+            result += 1
+    return result
 
-different_list = ["hello"] * 5
 
-print(len(different_list))
-print(different_list)
-
-list = [1, 2, 3, 4, 5, 6]
-
-for i in range(0, len(list)):
-    print(list[i])
-print(list[0:3])
+print(check([10, 20, 30, 40, 30, 20]))
