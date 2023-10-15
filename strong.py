78d0d715d9b0be3c8ea1f6f1a0d39e6d25aa14ed
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-14 21:08:01 -0600

    Strong solved by cheating!!!!!!!

--- strong.py
+++ strong.py
@@ -1,15 +1,16 @@
-def strong(num):
-    str_num = str(num)
-    list1 = list(str_num)
-    fac = 1
-    result = 0
-    for x in list1:
-        for i in range(1, int(x) + 1):
-            fac *= i
-            print(i, "i")
-            print(fac, "fac")
-            result += fac
-    print(result, "result")
+def strong_number(num):
+    number_list = [int(n) for n in str(num)]
+    sum_number = 0
+    for digit in number_list:
+        fact = 1
+        for i in range(1, digit + 1):
+            fact *= i
+        sum_number += fact
+    print(sum_number)
+    if sum_number == num:
+        print(True)
+    else:
+        print(False)
 
 
-strong(145)
+strong_number(40585)
