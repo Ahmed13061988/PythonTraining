e8cc14bc27a6d23478a481ee6a514ea92f26504b
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-14 21:44:20 -0600

    Set in python

---
+++ sets.py
@@ -0,0 +1,10 @@
+items = {"Carrot", "Apple", "Tomato"}
+
+print(items)
+
+empty_set = set()  # Creating an empty set
+print(empty_set)
+
+any_set = set()
+any_set.add("Hello") # adding to the set
+print(any_set)
