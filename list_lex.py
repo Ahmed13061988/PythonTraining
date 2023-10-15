6d9629c7f3a0acf65ece19e85b2aa65bfffc70f4
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-14 18:34:54 -0600

    Lists in python

--- list_lex.py
+++ list_lex.py
@@ -1,6 +1,12 @@
 ordinary_list = []
 
-different_list = ["hello"]*5
+different_list = ["hello"] * 5
 
 print(len(different_list))
 print(different_list)
+
+list = [1, 2, 3, 4, 5, 6]
+
+for i in range(0, len(list)):
+    print(list[i])
+print(list[0:3])
