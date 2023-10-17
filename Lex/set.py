1edd344e23e517fbadacf604d0cf84d3b249824a
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-16 19:37:49 -0600

    set union() method. intersection() method.

--- Lex/set.py
+++ Lex/set.py
@@ -13,3 +13,10 @@ my_set2 = {"Salwa", "Hassan", "Muhammed", "Hussein"}
 
 my_family = my_set1.union(my_set2)
 print(my_family)
+
+
+my_set4 = {1,2,3,4,5}
+my_set5 = {2,5,4,3}
+
+common_set = my_set4.intersection(my_set5)
+print(common_set)
