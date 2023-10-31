9c0dc6f99c18c6947ab3ee0d232163ee2f20fed3
Ahmed Hussein <wolktheline@yahoo.com>
2023-10-31 17:59:20 -0600

    Handling files in python.

--- Lex/file_handeling/files.py
+++ Lex/file_handeling/files.py
@@ -1,1 +1,7 @@
-new = "any"
+# Syntax of opening files.
+# file_object = open(file_name_path [, access_mode])
+# file_name_path is the filename or path of the file to be opened.
+# access_mode is the mode in which the file has to be opened which is optional. If not specified, default value will be read.
+# file_object is the object returned by open method which is used for further file operations.
+# Example :
+# fhr=open("data.txt","r")
