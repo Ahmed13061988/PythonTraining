2cd58568dfae2b32554c7b249e86066910b19d07
Ahmed Hussein <wolktheline@yahoo.com>
2023-11-01 18:16:29 -0600

    Handling exception inside a function.

--- Lex/exceptions/exception_handling.py
+++ Lex/exceptions/exception_handling.py
@@ -4,10 +4,14 @@ def calculate_expenditure(list_of_expenditure):
         for expenditure in list_of_expenditure:
             total += expenditure
         print(total)
+    except TypeError:
+        print("The type is wrong")
+    except ZeroDivisionError:
+        print("The value been divided by zero")
     except:
         print("Some error occurred")
     print("Returning back from function.")
 
 
-list_of_values = [100, 200, 300, "400", 500]
+list_of_values = [100, 200, 300, "A", 500]
 calculate_expenditure(list_of_values)
