bab361eec944dfe0e3060e7d23ebdc98aa9f762a
Ahmed Hussein <wolktheline@yahoo.com>
2023-11-16 19:27:54 -0700

    Using Destructor, to delete any object.

--- OOP/intoduction.py
+++ OOP/intoduction.py
@@ -4,8 +4,13 @@ class Mobile:
         self.brand = brand
         self.price = price
 
+    def __del__(self):
+        print("object has been deleted")
+
 
 mob1 = Mobile("Apple", 1119)
 mob2 = Mobile("Samsung", 1200)
+print(mob1.price, mob2.brand)
+del mob2
 
-print(mob1.price, mob2)
+
