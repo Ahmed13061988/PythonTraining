271ffe7235bfc391be9872eb7969dc3e850e9edf
Ahmed Hussein <wolktheline@yahoo.com>
2023-11-25 16:25:16 -0700

    Adding __ before the attribute, that will make the attribute not editable in the code.

--- OOP/encapsulation.py
+++ OOP/encapsulation.py
@@ -3,16 +3,16 @@ class Customer:
         self.cust_id = cust_id
         self.name = name
         self.age = age
-        self.wallet_balance = wallet_balance
+        self.__wallet_balance = wallet_balance
 
     def update_balance(self, amount):
         if amount < 1000 and amount > 0:
-            self.wallet_balance += amount
+            self.__wallet_balance += amount
 
     def show_balance(self):
-        print("The balance is ", self.wallet_balance)
+        print("The balance is ", self.__wallet_balance)
 
 
 c1 = Customer(100, "Gopal", 24, 1000)
-c1.update_balance(500)
+c1.wallet_balance = 10000000
 c1.show_balance()
