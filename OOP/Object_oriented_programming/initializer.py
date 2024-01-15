0f85009205769339231b3e394338f4ac9802110c
Ahmed Hussein <wolktheline@yahoo.com>
2024-01-15 15:16:02 -0700

    Methods for instances of class.

--- OOP/Object_oriented_programming/initializer.py
+++ OOP/Object_oriented_programming/initializer.py
@@ -1,11 +1,17 @@
 class Item():
-    def __init__(self, name, price, quantity):
-        print("I'm created")
+    def __init__(self, name, price, quantity=0):
         self.name = name
         self.price = price
         self.quantity = quantity
 
+    def calculate_total(self):
+        return self.price * self.quantity
 
-item1 = Item("Phone", 200, 5)
+
+item1 = Item("Phone", 200, 1)
 item2 = Item("Laptop", 1000, 3)
 
+item2.has_numpad = False
+
+print(item1.calculate_total())
+print(item2.calculate_total())
