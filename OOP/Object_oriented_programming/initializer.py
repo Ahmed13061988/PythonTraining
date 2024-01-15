755ffd1f3a479cb0b889fd8c783b43b37ddc8725
Ahmed Hussein <wolktheline@yahoo.com>
2024-01-15 15:28:39 -0700

    Assert statement with provided error string.

--- OOP/Object_oriented_programming/initializer.py
+++ OOP/Object_oriented_programming/initializer.py
@@ -1,8 +1,8 @@
 class Item():
     def __init__(self, name: str, price: float, quantity: int = 0):
         # checking if the passed arguments are valid
-        assert price > 0
-        assert quantity > 0
+        assert price > 0, f"Price {price} is not greater than zer0!"
+        assert quantity > 0, f"Quantity {quantity} is not greater than zero!"
         self.name = name
         self.price = price
         self.quantity = quantity
